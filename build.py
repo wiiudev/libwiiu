0691dcb92947f5fc66d510df2f6c7386b1ae9460
Jeffrey Crowell <crowell@bu.edu>
2015-03-04 19:47:46 -0500

    make build.py linux friendly
    downcase "make" and use fs independent way of joining dirs

--- build.py
+++ build.py
@@ -8,24 +8,25 @@ def main():
 	ar3=""
 	ver=""
 	rootDir=os.path.dirname(os.path.realpath(sys.argv[0]))
-	frameworkDir=rootDir+"\\framework"
-	libsDir=rootDir+"\\libwiiu"
-	wwwDir=rootDir+"\\www"
+	frameworkDir=os.path.join(rootDir,"framework")
+	libsDir=os.path.join(rootDir,"libwiiu")
+	wwwDir=os.path.join(rootDir,"www")
 	try:
 		ar1=sys.argv[1]
 		ar1=sys.argv[2]
 	except:
 		pass
-	projectDir=rootDir+"\\"+ar1
+	projectDir=os.path.join(rootDir,ar1)
 	platformStr=platform.system().lower()
 	print("Building for your "+platformStr+" platform...")
-	os.mkdir(wwwDir)
+        if not os.path.exists(wwwDir):
+	    os.mkdir(wwwDir)
 	os.chdir(frameworkDir)
-	os.system("Make")
+	os.system("make")
 	os.chdir(libsDir)
-	os.system("Make")
+	os.system("make")
 	os.chdir(projectDir)
-	os.system("Make")
+	os.system("make")
 	os.system("python "+frameworkDir+"/generate_html.py "+projectDir+"/bin/code"+str(500)+".bin "+str(500)+" "+wwwDir)
 	os.system("python "+frameworkDir+"/generate_html.py "+projectDir+"/bin/code"+str(410)+".bin "+str(410)+" "+wwwDir)
 	os.system("python "+frameworkDir+"/generate_html.py "+projectDir+"/bin/code"+str(400)+".bin "+str(400)+" "+wwwDir)
