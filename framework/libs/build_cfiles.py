1b9d4b03a885c961120f2265287be9ea76ce3b57
Marionumber1 <gksharkboy@gmail.com>
2015-05-02 11:04:52 -0400

    Fake dynlibs build successfully

--- framework/libs/build_cfiles.py
+++ framework/libs/build_cfiles.py
@@ -10,5 +10,5 @@ for lib in libnames:
     ofile = lib[:-3] + 'o'
     sofile = lib[:-3] + "so"
     os.system("powerpc-eabi-gcc -fPIC -ffreestanding -nostdinc -g -c " + cfile)
-    os.system("powerpc-eabi-gcc -shared -o lib" + sofile + " " + ofile)
+    os.system("powerpc-eabi-gcc -shared -fPIC -Wl,-N -o lib" + sofile + " " + ofile)
     
