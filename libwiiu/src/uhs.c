8c6c9627603ca86e4144cd67cae106ecd4d68e91
Marionumber1 <gksharkboy@gmail.com>
2015-05-16 22:10:26 -0400

    Implement UhsOpenController() and UhsQueryInterfaces()

---
+++ libwiiu/src/uhs.c
@@ -0,0 +1,30 @@
+#include "coreinit.h"
+#include "uhs.h"
+
+/* Open a specific controller under /dev/uhs */
+int UhsOpenController(int controller_num)
+{
+	/* Symbol loading */
+	unsigned int coreinit_handle;
+	OSDynLoad_Acquire("coreinit.rpl", &coreinit_handle);
+	int (*IOS_Open)(char *path, int mode);
+	OSDynLoad_FindExport(coreinit_handle, false, "IOS_Open", &IOS_Open);
+
+	/* Build the path name and return a handle */
+	char path[32];
+	__os_snprintf(path, 32, "/dev/uhs/%d", controller_num);
+	return IOS_Open(path, 0);
+}
+
+/* Determine which USB device interfaces are plugged in and available */
+int UhsQueryInterfaces(int uhs_handle, UhsInterfaceFilter *filter, UhsInterfaceProfile *profiles, int max_profiles)
+{
+	/* Symbol loading */
+	unsigned int coreinit_handle;
+	OSDynLoad_Acquire("coreinit.rpl", &coreinit_handle);
+	int (*IOS_Ioctl)(int fd, int request, void *inbuf, int inlen, void *outbuf, int outlen);
+	OSDynLoad_FindExport(coreinit_handle, false, "IOS_Ioctl", &IOS_Ioctl);
+
+	/* Perform the ioctl() request */
+	return IOS_Ioctl(uhs_handle, 0x11, filter, 0x10, profiles, max_profiles * 0x16c);
+}
