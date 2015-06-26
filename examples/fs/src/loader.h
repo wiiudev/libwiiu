cfff313e53e08567d4fe39bb72f6ea5752779763
Marionumber1 <gksharkboy@gmail.com>
2015-06-25 23:43:01 -0400

    Add FS test

---
+++ examples/fs/src/loader.h
@@ -0,0 +1,10 @@
+#ifndef LOADER_H
+#define LOADER_H
+
+#include "../../../libwiiu/src/coreinit.h"
+#include "../../../libwiiu/src/types.h"
+
+void _start();
+
+void _entryPoint();
+#endif /* LOADER_H */
