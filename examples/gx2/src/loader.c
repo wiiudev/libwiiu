82421f21d7a1c43ed93c15d92d2030df58ed6876
Marionumber1 <gksharkboy@gmail.com>
2015-06-27 03:25:55 -0400

    GX2 data area is much later

--- examples/gx2/src/loader.c
+++ examples/gx2/src/loader.c
@@ -4,7 +4,7 @@ void _start()
 {
 	/* Get the framebuffer of the TV or DRC */
 	int dev = 0;
-	unsigned int *fb = *((unsigned int*)(0x10014DC0 + 0x304 + (dev * 0x20)));
+	unsigned int *fb = *((unsigned int*)(0x1032AEC0 + 0x304 + (dev * 0x20)));
 
 	char buf[256];
 	__os_snprintf(buf, 256, "0x%08X", fb);
