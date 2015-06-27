141fc0ee3c33ce0b4c967851e2f27f95cd9a9dcc
Marionumber1 <gksharkboy@gmail.com>
2015-06-27 19:18:01 -0400

    Making threads exit and using OSScreenInit() makes screen corruption, with green fill

--- examples/gx2/src/loader.c
+++ examples/gx2/src/loader.c
@@ -7,7 +7,7 @@ void _start()
 	OSDynLoad_Acquire("coreinit.rpl", &coreinit_handle);
 	OSDynLoad_Acquire("gx2.rpl", &gx2_handle);
 
-	/* OS memory and IM functions
+	/* OS memory and IM functions */
 	void* (*memset)(void *dest, uint32_t value, uint32_t bytes);
 	void* (*OSAllocFromSystem)(uint32_t size, int align);
 	void (*OSFreeToSystem)(void *ptr);
@@ -19,7 +19,7 @@ void _start()
 	OSDynLoad_FindExport(coreinit_handle, 0, "OSFreeToSystem", &OSFreeToSystem);
 	OSDynLoad_FindExport(coreinit_handle, 0, "IM_Open", &IM_Open);
 	OSDynLoad_FindExport(coreinit_handle, 0, "IM_Close", &IM_Close);
-	OSDynLoad_FindExport(coreinit_handle, 0, "IM_SetDeviceState", &IM_SetDeviceState);*/
+	OSDynLoad_FindExport(coreinit_handle, 0, "IM_SetDeviceState", &IM_SetDeviceState);
 
 	/* GX2 display functions */
 	void (*GX2SetDRCEnable)(bool enable);
@@ -31,17 +31,17 @@ void _start()
 	OSDynLoad_FindExport(gx2_handle, 0, "GX2SwapScanBuffers", &GX2SwapScanBuffers);
 	OSDynLoad_FindExport(gx2_handle, 0, "GX2Invalidate", &GX2Invalidate);
 
-	/* Cause the other browser threads to exit
+	/* Cause the other browser threads to exit */
 	int fd = IM_Open();
 	void *mem = OSAllocFromSystem(0x100, 64);
 	memset(mem, 0, 0x100);
 	IM_SetDeviceState(fd, mem, 3, 0, 0); 
 	IM_Close(fd);
-	OSFreeToSystem(mem);*/
+	OSFreeToSystem(mem);
 
-	/* Wait for the threads to exit
+	/* Wait for the threads to exit */
 	unsigned int t1 = 0x1FFFFFFF;
-	while(t1--);*/
+	while(t1--);
 
 	/* Get the framebuffer of the TV or DRC */
 	int dev = 1;
@@ -49,12 +49,14 @@ void _start()
 	uint32_t *fb_back = fb_front + (864 * 480);
 
 	/* Write blue to the screen and swap the buffers */
-	for (int i = 0; i < 864 * 320; i++) *fb_back++ = 0xFF0000FF;
+	for (int i = 0; i < 864 * 320; i++) *fb_back++ = 0x00FF00FF;
 	GX2Invalidate(0x40, fb_back, 864 * 320 * 4);
 	GX2WaitForVsync();
 	GX2SwapScanBuffers();
 
-	OSFatal("HI");
+	void (*OSScreenInit)();
+	OSDynLoad_FindExport(coreinit_handle, 0, "OSScreenInit", &OSScreenInit);
+	OSScreenInit();
 
 	while(1);
 }
