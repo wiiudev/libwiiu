93874f05b009be7fdf11327b2ef284e50f2b94f4
Marionumber1 <gksharkboy@gmail.com>
2016-01-15 21:50:52 -0500

    Get parts of FS example working, FSReadDir() crashes

--- examples/fs/src/loader.c
+++ examples/fs/src/loader.c
@@ -1,45 +1,92 @@
 #include "loader.h"
 
+/* Start a thread for the FS funcs */
 void _start()
 {
     /* Get a handle to coreinit.rpl */
 	uint32_t coreinit_handle;
 	OSDynLoad_Acquire("coreinit.rpl", &coreinit_handle);
 
-	/* Memory allocation and FS functions */
+	/* Memory allocation functions */
 	void* (*OSAllocFromSystem)(uint32_t size, int align);
-	int (*FSInit)();
+	OSDynLoad_FindExport(coreinit_handle, 0, "OSAllocFromSystem", &OSAllocFromSystem);
+
+	/* OS thread functions */
+	bool (*OSCreateThread)(void *thread, void *entry, int argc, void *args, uint32_t *stack, uint32_t stack_size, int priority, uint16_t attr);
+	int (*OSResumeThread)(void *thread);
+	OSDynLoad_FindExport(coreinit_handle, 0, "OSCreateThread", &OSCreateThread);
+	OSDynLoad_FindExport(coreinit_handle, 0, "OSResumeThread", &OSResumeThread);
+
+	/* Make a thread to run the FS functions */
+	OSContext *thread = (OSContext*)OSAllocFromSystem(0x1000, 8);
+	uint32_t *stack = (uint32_t*)OSAllocFromSystem(0x1000, 0x20);
+	if (!OSCreateThread(thread, &fs_test, 0, NULL, stack + 0x400, 0x1000, 0, 0x2 | 0x8)) OSFatal("Failed to create thread");
+	OSResumeThread(thread);
+
+	/* Infinite loop */
+	while(1);
+}
+
+/* FS testing */
+void fs_test()
+{
+    /* Get a handle to coreinit.rpl */
+	uint32_t coreinit_handle;
+	OSDynLoad_Acquire("coreinit.rpl", &coreinit_handle);
+
+	/* Memory allocation functions */
+	uint32_t *memalign, *freemem;
+	OSDynLoad_FindExport(coreinit_handle, 1, "MEMAllocFromDefaultHeapEx", &memalign);
+	OSDynLoad_FindExport(coreinit_handle, 1, "MEMFreeToDefaultHeap", &freemem);
+	void* (*MEMAllocFromDefaultHeapEx)(uint32_t size, int align) = (void* (*)(uint32_t,int))*memalign;
+	void (*MEMFreeToDefaultHeap)(void *ptr) = (void (*)(void*))*freemem;
+
+	/* FS functions */
+	void (*FSInit)();
+	void (*FSShutdown)();
 	int (*FSAddClient)(void *client, int unk1);
 	int (*FSInitCmdBlock)(void *cmd);
 	int (*FSOpenDir)(void *client, void *cmd, char *path, uint32_t *dir_handle, int unk1);
 	int (*FSReadDir)(void *client, void *cmd, uint32_t dir_handle, void *buffer, int unk1);
-	OSDynLoad_FindExport(coreinit_handle, 0, "OSAllocFromSystem", &OSAllocFromSystem);
 	OSDynLoad_FindExport(coreinit_handle, 0, "FSInit", &FSInit);
+	OSDynLoad_FindExport(coreinit_handle, 0, "FSShutdown", &FSShutdown);
 	OSDynLoad_FindExport(coreinit_handle, 0, "FSAddClient", &FSAddClient);
 	OSDynLoad_FindExport(coreinit_handle, 0, "FSInitCmdBlock", &FSInitCmdBlock);
 	OSDynLoad_FindExport(coreinit_handle, 0, "FSOpenDir", &FSOpenDir);
 	OSDynLoad_FindExport(coreinit_handle, 0, "FSReadDir", &FSReadDir);
 
-	/* Initialize the FS library */
-	FSInit();
+	/* Shutdown and reinitialize the FS library 
+	FSShutdown();
+	FSInit();*/
 
 	/* Set up the client and command blocks */
-	void *client = OSAllocFromSystem(0x1700, 0x20);
-	void *cmd = OSAllocFromSystem(0xA80, 0x20);
-	if (!(client && cmd)) OSFatal("Failed to allocate client and command block");
+	void *client = MEMAllocFromDefaultHeapEx(0x1700, 0x20);
+	void *cmd = MEMAllocFromDefaultHeapEx(0xA80, 0x20);
+	if (!client || !cmd) OSFatal("Failed to allocate client and command block");
+	//((void (*)())0x101cd70)();
+	*((uint32_t*)0x10174F88) = 0;
+	//((void (*)())0x101cd70)();
 	FSAddClient(client, 0);
+	//((void (*)())0x101cd70)();
 	FSInitCmdBlock(cmd);
 
 	/* Open /vol/save */
 	uint32_t dir_handle;
 	FSOpenDir(client, cmd, "/vol/save", &dir_handle, -1);
 
+	//char buf[256];
+	//__os_snprintf(buf, 256, "Handle: 0x%08X", dir_handle);
+	//OSFatal(buf);
+
+	//((void (*)())0x101cd70)();
+
 	/* Start reading its directory entries */
-	uint32_t *buffer = OSAllocFromSystem(0x200, 0x20);
+	uint32_t *buffer = MEMAllocFromDefaultHeapEx(0x200, 0x20);
 	while (1)
 	{
 		/* Read the directory entry */
 		int ret = FSReadDir(client, cmd, dir_handle, buffer, -1);
+		((void (*)())0x101cd70)();
 		if (ret != 0) break;
 
 		/* Get the attributes, size, and name */
@@ -47,9 +94,19 @@ void _start()
 		uint32_t size = buffer[1];
 		char *name = (char*)&buffer[25];
 
+		((void (*)())0x101cd70)();
+
 		/* Print it out */
 		char buf[256];
-		__os_snprintf("%s\t%d bytes\t%s", name, size, (attr & 0x80000000) ? "Directory" : "File");
+		__os_snprintf(buf, 256, "%s\t%d bytes\t%s\n", name, size, (attr & 0x80000000) ? "Directory" : "File");
 		OSFatal(buf);
 	}
+
+	OSFatal("Failed to read any dirents");
+}
+
+void *memset(void *dst, int value, uint32_t len)
+{
+	for (int i = 0; i < len; i++) ((uint8_t*)dst)[i] = (uint8_t)value;
+	return dst;
 }
--- examples/fs/src/loader.h
+++ examples/fs/src/loader.h
@@ -4,7 +4,13 @@
 #include "../../../libwiiu/src/coreinit.h"
 #include "../../../libwiiu/src/types.h"
 
+/* Start a thread for the FS funcs */
 void _start();
 
-void _entryPoint();
+/* FS testing */
+void fs_test();
+
+/* Set a range of memory to a specific value */
+void *memset(void *dst, int value, uint32_t len);
+
 #endif /* LOADER_H */
